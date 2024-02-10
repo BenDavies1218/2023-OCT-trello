be4f4bf416baaeff4691d9cbc1552015e51c81c0
BenDavies1218 <14972@coderacademy.edu.au>
2024-02-10 11:52:13 +1000

    feature/authentication

--- models/user.py
+++ models/user.py
@@ -3,10 +3,11 @@ from init import db, ma
 
 class User(db.Model):
     __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
 
@@ -15,5 +16,5 @@ class UserSchema(ma.Schema):
         fields = ("id", "name", "email", "password", "is_admin")
 
 
-user_schema = UserSchema(exclude=["password"])
-users_schema = UserSchema(many=True, exclude=["password"])
+user_schema = UserSchema(exclude=["password"])  # {}
+users_schema = UserSchema(many=True, exclude=["password"])  # [{}, {}, {}]
