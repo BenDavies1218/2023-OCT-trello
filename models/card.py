63ef6e2b6eb90680f20f49c3d7d976a001255f30
BenDavies1218 <14972@coderacademy.edu.au>
2024-02-17 11:20:23 +1000

    feature/comment model complete

--- models/card.py
+++ models/card.py
@@ -4,22 +4,37 @@ from marshmallow import fields
 
 class Card(db.Model):
     __tablename__ = "cards"
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text)
     date = db.Column(db.Date)
     status = db.Column(db.String)
     priority = db.Column(db.String)
+
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     user = db.relationship("User", back_populates="cards")
+    comments = db.relationship("comment", back_populates="card")
 
 
 class CardSchema(ma.Schema):
+
     user = fields.Nested("UserSchema", only=["name", "email"])
 
+    comment = fields.List(fields.Nested("CommentSchema", exclude=["card"]))
+
     class Meta:
-        fields = ("id", "title", "description", "date", "status", "priority", "user")
+        fields = (
+            "id",
+            "title",
+            "description",
+            "date",
+            "status",
+            "priority",
+            "user",
+            "comments",
+        )
         ordered = True
 
 
